59f22af1e2f02e09c4b476b0938771940af0611d
Samuel Fortier-Galarneau <sgalarneau@digium.com>
2014-10-14 23:06:04 -0500

    Voicemail Main application.

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * Voicemail Main application bootstrap.
+ *
+ * @module app
+ * @copyright 2014, Digium, Inc.
+ * @license Apache License, Version 2.0
+ * @author Samuel Fortier-Galarneau <sgalarneau@digium.com>
+ */
+
+'use strict';
+
+var app = require('./lib/voicemail-main.js');
+
+app.create();
